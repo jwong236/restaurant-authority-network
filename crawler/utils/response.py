4781f24ce7207163f5c918f031a1cc833a7d7e40
Jacob Wong <jacobw5@uci.edu>
2023-07-05 20:54:52 -0700

    Update response object dumping to prevent exceptions

--- crawler/utils/response.py
+++ crawler/utils/response.py
@@ -1,5 +1,6 @@
-import json
 import os
+import json
+from crawler.utils import get_urlhash
 
 class Response(object):
     def __init__(self, raw_url=None, url=None, status_code=None, headers=None, content=None, text=None, error=None):
@@ -13,7 +14,11 @@ class Response(object):
         self.json_data = {"url": url, "text": text}
         self.filename = None
 
-    def to_json(self, directory):
-        self.filename = os.path.join(directory, f'{self.raw_url.replace("/", "_")}.json')
-        with open(self.filename, 'w') as file:
-            json.dump(self.json_data, file)
+    def to_json(self, directory="corpus_directory"):
+        os.makedirs(directory, exist_ok=True)
+        self.filename = os.path.join(directory, f'{get_urlhash(self.raw_url)}.json')
+        try:
+            with open(self.filename, 'w') as file:
+                json.dump(self.json_data, file)
+        except Exception as e:
+            print(f"An error occurred when writing to file: {e}")
